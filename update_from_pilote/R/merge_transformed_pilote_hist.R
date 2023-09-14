7de2c1598c15b9d1c3e1f88070972fff484df74c
Colin DUGEAI <colin.dugeai@gmail.com>
2023-09-14 15:14:11 +0000

    combine rows from hist data and new. WARN: no deduplication
    Deduplication to implement

--- update_from_pilote/R/merge_transformed_pilote_hist.R
+++ update_from_pilote/R/merge_transformed_pilote_hist.R
@@ -1,12 +1,25 @@
 library(magrittr)
+library(tidyr)
 
+
+# This function combines hist_data and pilote_data.
+tf_combine <- function(transformed_data_pilote_, data_hist_) {
+  data_hist_ %>%
+    select(
+      indic_id, enforce_zone_id, code_insee= enforce_zone_id, metric_enforce_date, 
+      indic_vi, indic_va, indic_vc_inter=indic_vc,
+      maille, indic_ta_inter=indic_ta
+    ) %>%
+    bind_rows(transformed_data_pilote_)
+  
+  
+}
 
 # This demo function simply sorts hist_data and ignores pilote_data.
 tf_sort_hist_data <- function(transformed_data_pilote_, data_hist_) {
   data_hist_ %>%
     arrange(indic_id, enforce_zone_id, metric_enforce_date)
 }
-
 
 #' Merge transformed data from pilote with historic data
 #' 
@@ -18,7 +31,5 @@ tf_sort_hist_data <- function(transformed_data_pilote_, data_hist_) {
 #'
 #' @examples
 merge_transformed_pilote_hist <- function(transformed_data_pilote_, data_hist_) {
-  tf_sort_hist_data(transformed_data_pilote_, data_hist_)
+  tf_combine(transformed_data_pilote_, data_hist_)
 }
-
-
