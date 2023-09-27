b4e97fec390b40b49f87f93ee641c5b9b92ce91e
Colin Dugeai <colin.dugeai@gmail.com>
2023-09-27 13:03:16 +0200

    cli.delete: use deleteResourceDatasetPattern
    to delete resource of dataset based on their title

--- cli/app.ts
+++ cli/app.ts
@@ -1,22 +1,45 @@
 #!/usr/bin/env node
 import yargs from 'yargs'
 import {hideBin} from 'yargs/helpers'
-import {Datagouv} from 'datagouv-ts'
+import {Datagouv, DatagouvResourceCustom} from 'datagouv-ts'
+const branchName = require('current-git-branch');
+import {config} from './config'
 
-const argss =yargs(hideBin(process.argv))
+
+
+//@ts-ignore
+const datagouvConfig : any = config.branch[branchName()].datagouv;
+
+yargs(hideBin(process.argv))
+  .parserConfiguration({"boolean-negation": false})
   .command('delete <pattern>', 'Delete resource based on their title', (yargs) => {
     return yargs
       .positional('pattern', {
-        describe: 'pattern of resorces title to delete',
+        describe: 'pattern of resources title to delete',
         type: "string",
       })
-  })
+  },  async (argv_) => {
+    let deleted_resources: DatagouvResourceCustom[] = await Datagouv.deleteResourceDatasetPattern(
+        datagouvConfig.DATASET, datagouvConfig.API_BASE_URL, 
+        process.env[datagouvConfig.API_KEY_VAR]!, 
+        argv_.pattern!, Boolean(argv_["no-dry-run"]),
+        Boolean(argv_.verbose)
+        )
+    console.log({deleted_resources: deleted_resources.map(e=> e.title)});
+    
+    }
+
+    
+  )
   .option('verbose', {
     alias: 'v',
     type: 'boolean',
     description: 'Run with verbose logging'
   })
-  .argv
-
-console.log(argss)
+  .option('no-dry-run', {
+    type: 'boolean',
+    default: false,
+    description: 'Do not run in dry-run mode. By default, all commands run in dry-mode.'
+  })
+  .parse()
 
